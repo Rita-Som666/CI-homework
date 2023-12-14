5e19d8fc58f31c143d7d37de3edc9966f9f2bac4
Tatiana Titova <ritasom666@gmail.com>
2023-12-14 17:33:38 +0400

    add homework

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,3 +1,5 @@
+//У меня не получилось сделать так, чтобы сборка падала от недостаточного покрытия
+
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,18 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-15, -10, -1};
+        long expected = -1;
 
         long actual = service.findMax(incomesInBillions);
 
